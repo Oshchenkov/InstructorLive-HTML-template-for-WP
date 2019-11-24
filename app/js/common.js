664bf1b004363b16821dab559fb7055be6975654
Oshchenkov <oshchenkovv@gmail.com>
2019-11-24 22:50:28 +0200

    add new feature for mobile menu

--- app/js/common.js
+++ app/js/common.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
   const $togleBtn = $("#top-navbar-btn");
   const $header = $(".header ");
-  const $mobileContainer = $(".mobile-nav-container");
+  const $mobileContainer = $("#mobile-container");
   let openMenuState = false;
 
   $togleBtn.click(e => {
@@ -11,19 +11,36 @@ $(document).ready(function() {
   });
 
   const toggleMobileMenu = e => {
-    const $btnContainer = $(e.currentTarget);
-    console.log("TCL: btnContainer", $btnContainer);
     console.log(openMenuState);
 
     if (openMenuState) {
       $header.removeClass("open");
-      $btnContainer.removeClass("open");
       $mobileContainer.slideToggle();
     } else {
       $header.addClass("open");
-      $btnContainer.addClass("open");
       $mobileContainer.slideToggle();
     }
     openMenuState = !openMenuState;
   };
+
+  // https://jonsuh.com/hamburgers/
+  var forEach = function(t, o, r) {
+    if ("[object Object]" === Object.prototype.toString.call(t))
+      for (var c in t)
+        Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+    else for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t);
+  };
+
+  var hamburgers = document.querySelectorAll(".hamburger");
+  if (hamburgers.length > 0) {
+    forEach(hamburgers, function(hamburger) {
+      hamburger.addEventListener(
+        "click",
+        function() {
+          this.classList.toggle("is-active");
+        },
+        false
+      );
+    });
+  }
 });
